1b436bf1fa46c6a0a1b9ebba7f5dc04206448ef7
muhammad ingar <muhammadingar05@gmail.com>
2024-09-03 11:48:45 +0100

    add bubble sort

---
+++ sort/bubble/bubble-sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(arr)-> list:
+    if len(arr) <= 0:
+        return []
+    for index in range(0,len(arr)):
+        correct = False
+        for curr in range(0,len(arr)-index-1):
+            if arr[curr] >= arr[curr+1]:
+                temp = arr[curr+1]
+                arr[curr+1] = arr[curr]
+                arr[curr] = temp
+    return arr
+
+if __name__ == "__main__":
+    array = []
+    while True:
+        temp = input("Enter a value into the array (press enter to finish): \n")
+        try:
+            if temp:
+                array.append(int(temp))
+            else:
+                break
+        except:
+            print("integers only please")
+    print(bubble_sort(array))
